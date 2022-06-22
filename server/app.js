0547ea0da479907198df9d2b70e77d7add900749
so191925 <so191925@gmail.com>
2022-06-22 23:48:23 +0900

    feat: 리스트 라우터 경로 설정

--- server/app.js
+++ server/app.js
@@ -5,6 +5,10 @@ const app = express();
 const path = require("path");
 const PORT = 3333;
 
+const data =  [
+    { title: "양희준", content: "코드스테이츠" },
+    { title: "심소영", content: "코드스테이츠" },
+    { title: "이유진", content: "코드스테이츠" }];
 
 
 app.use(express.json());
@@ -13,6 +17,9 @@ app.use(logger("tiny"));
 app.use(express.static(path.join(__dirname, "../client/build")));
 //라우터 작성
 
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
+
+app.get('/list', (req,res) => res.status(200).json(data))
 
 app.listen(PORT, () => console.log('서버실행중입니다.'))
