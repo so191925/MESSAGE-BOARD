242c37e0228ac5b2083830707168afe1f932a4c8
so191925 <so191925@gmail.com>
2022-06-22 23:53:12 +0900

    feat: 라우트 경로설정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import{Routes, Route} from 'react-router-dom'
+import List from "./components/List";
+import Update from "./components/Update";
+import Navi from "./components/Navi";
+import ListItem from "./components/ListItem";
+
 
 function App() {
     return (
         <React.Fragment>
-        Hello React App
+            <Navi />
+            <Routes>
+                <Route path ='/' element={<List/>}/>
+                <Route path ='/new' element= {<Update/>}/>
+            </Routes>
         </React.Fragment>
     )
 }
