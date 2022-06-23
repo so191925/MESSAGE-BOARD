82ffc5f1d98abf4820a58edb316da567bbd57818
so191925 <so191925@gmail.com>
2022-06-23 23:20:01 +0900

    feat: useState & useEffect 작성

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ListItem from './ListItem';
 import axios from 'axios';
+import PageList from './PageList';
 
 function List() {
-    const handleOnClick = () => {
-        axios.get('/list')
-        .then((res) => console.log(res));
-    }
-
-    const makeListItems = () => {
-        
-        const data = [
-            { title: '소영', content: '코드스테이츠'},
-            { title: '소영', content: '코드스테이츠'},
-            { title: '소영', content: '코드스테이츠'}
-        ];
+    const [page, setPage] = useState(1);
+    const [data, setData] = useState([]);
+    const makeListItems = (data = []) => {
         return data.map((item, index) =><ListItem key={index} title={item.title} content={item.content}/>)
     }
+    useEffect(() =>{
+        axios.get(`api/list/?page=${page}`)
+        .then((res) => setData(res.data));
+    }, [page]);
+
     return (
         <>
-        {makeListItems()}
-        <button onClick={handleOnClick}>통신</button>
+        {makeListItems(data)}
+        <PageList page={page} setPage={setPage}/>
         </>
     )
 }
