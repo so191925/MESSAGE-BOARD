cd09adde736449c8ed0ec37d6c9ddcaabe5a3212
so191925 <so191925@gmail.com>
2022-06-23 23:12:21 +0900

    feat: 게시판 목록 정제 기능 구현

--- server/app.js
+++ server/app.js
@@ -6,10 +6,24 @@ const path = require("path");
 const PORT = 3333;
 
 const data =  [
-    { title: "양희준", content: "코드스테이츠" },
-    { title: "심소영", content: "코드스테이츠" },
-    { title: "이유진", content: "코드스테이츠" }];
+    { title: "양희준1", content: "코드스테이츠" },
+    { title: "심소영1", content: "코드스테이츠" },
+    { title: "이유진1", content: "코드스테이츠" },
+    { title: "양희준2", content: "코드스테이츠" },
+    { title: "심소영2", content: "코드스테이츠" },
+    { title: "이유진2", content: "코드스테이츠" },
+    { title: "양희준3", content: "코드스테이츠" },
+    { title: "심소영3", content: "코드스테이츠" },
+    { title: "이유진3", content: "코드스테이츠" },
+    { title: "양희준4", content: "코드스테이츠" },
+    { title: "심소영4", content: "코드스테이츠" },
+    { title: "이유진4", content: "코드스테이츠" },];
 
+const divide = (arr, num) => {
+    const result = [];
+    for(let i = 0; i <arr.length; i+= num)  result.push(arr.slice(i, i + num)); 
+    return result;
+ }
 
 app.use(express.json());
 app.use(cors());
@@ -20,6 +34,12 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
 
-app.get('/list', (req,res) => res.status(200).json(data))
+app.get('/api/list', (req,res) => {
+const temp = divide(data, 5);
+let { page } =req.query;
+if(!page) return res.status(200).json(temp[0]);
+ page = Number(page);
+ return res.status(200).json(temp[page -1]);
+});
 
 app.listen(PORT, () => console.log('서버실행중입니다.'))
